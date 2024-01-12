3a37e253ba48e0a3eebd1f5c60fb2a594cc9729b
Shubham Upadhyay <103402465+ShubhamVUpadhyay@users.noreply.github.com>
2024-01-12 11:27:52 +0530

    Delete MyBlog directory

--- MyBlog/Blog/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from tinymce.models import HTMLField
-# Create your models here.
-
-class Blog(models.Model):
-    blog_title=models.CharField(max_length=200)
-    blog_text=HTMLField()
-
-
